deb16406d37886483c957aa3c646480b20bbf8e7
John Pevien Laguindam <johnpevienwork@gmail.com>
2024-05-01 15:49:01 +0000

    Update tailwind utilities in globals.css and layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,194 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState, useRef } from "react";
+import { useChat } from "ai/react";
+import Head from "next/head";
+import { Toaster } from "react-hot-toast";
+import { Button, Select, TextField } from "@/components/form";
+import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
+
+export default function Chat() {
+  const onFormFinish = () => {
+    setLoading(false);
+  };
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    onFinish: onFormFinish,
+  });
+  const [loading, setLoading] = useState(false);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const submitForm = () => {
+    if (formRef.current) {
+      formRef.current.dispatchEvent(new Event("submit", { cancelable: true }));
+      setLoading(true);
+    }
+  };
+
+  // return (
+  //   <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+  //     {messages.map(m => (
+  //       <div key={m.id} className="whitespace-pre-wrap">
+  //         {m.role === 'user' ? 'User: ' : 'AI: '}
+  //         {m.content}
+  //       </div>
+  //     ))}
+
+  //     <form onSubmit={handleSubmit}>
+  //       <input
+  //         className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+  //         value={input}
+  //         placeholder="Say something..."
+  //         onChange={handleInputChange}
+  //       />
+  //     </form>
+  //   </div>
+  // );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <Head>
+        <title>Rephraser</title>
+      </Head>
+      <main
+        className={`flex  flex-col items-center min-h-[90vh] justify-start`}
+      >
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{ duration: 2000 }}
         />
-      </div>
+        <div className="bg-neutral w-full h-10 md:h-20 mb-10"></div>
+        <div className="max-w-prose px-8 md:px-0">
+          <div className="flex  flex-col items-center justify-start py-2">
+            <header className="mb-10 md:mb-16">
+              <h1 className="text-3xl font-bold underline leading-relaxed text-center">
+                Japanese Navigator
+              </h1>
+              {/* <p>A rephraser and grammar checker app powered by OpenAI API.</p> */}
+            </header>
+            <div className="w-full flex flex-col gap-5">
+              <div>
+                {/* <TextField
+                  className="rounded border-2 border-neutral w-full max-w-full"
+                  onChange={(e) => {
+                    setSentence(e.target.value);
+                  }}
+                  value={sentence}
+                /> */}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                <form onSubmit={handleSubmit} ref={formRef}>
+                  <TextField
+                    className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+                    value={input}
+                    placeholder="Say something..."
+                    onChange={handleInputChange}
+                  />
+                </form>
+              </div>
+              {/* <div className="flex flex-col sm:flex-row justify-between gap-5 items-center">
+                <p>Select tone:</p>
+                <Select
+                  className="rounded w-full max-w-full sm:max-w-xs"
+                  onChange={(e) => setVibe(e.target.value)}
+                  value={vibe}
+                  options={[
+                    "Professional",
+                    "Conversational",
+                    "Humorous",
+                    "Empatic",
+                    "Academic",
+                    "Simple",
+                    "Creative",
+                  ]}
+                />
+              </div> */}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <Button
+                onClick={submitForm}
+                disabled={loading || !input || input?.length < 7}
+              >
+                Send
+              </Button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+              <p className="text-xs text-center"></p>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <div className="w-full mt-10">
+              {messages && (
+                <>
+                  <hr />
+                  {/* <h3 className="text-center text-xl mt-3 mb-5 font-semibold">
+                    Rephrased Sentences
+                  </h3> */}
+                  {messages.map((m) => (
+                    <div key={m.id} className="whitespace-pre-wrap">
+                      {m.role === "user" ? "User: " : "AI: "}
+                      {m.content}
+                    </div>
+                  ))}
+                  {/* {rephrasedSentences.split("\n").map((sentence, index) => {
+                    if (sentence.length < 7) return;
+                    sentence = sentence
+                      .replace("- ", "")
+                      .replace(/^\d+\.\s/gm, "")
+                      .replace(/"/g, "")
+                      .trim();
+                    return (
+                      <Card
+                        text={sentence}
+                        key={index}
+                        className="text-center w-full mb-5"
+                        onClick={() => {
+                          navigator.clipboard.writeText(sentence);
+                          toast("Sentence have been copied to clipboard", {
+                            icon: "✂️",
+                          });
+                        }}
+                      />
+                    );
+                  })} */}
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="container mx-auto max-w-prose">
+        <p className="text-center text-sm text-gray-400">
+          This app uses AI and may produce inaccurate results
+        </p>
+        <hr className="mb-5 mt-2" />
+        <div className="flex flex-row justify-between items-center mb-8">
+          <div>
+            <p>
+              Powered by{" "}
+              <a
+                href="https://vercel.com/"
+                className="underline"
+                rel="noreferrer"
+              >
+                Vercel
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-row gap-3">
+            <a
+              href="https://github.com/JohnPevien/rephraser
+            "
+              rel="noreferrer"
+            >
+              <AiFillGithub size={"2em"} />
+            </a>
+            <a
+              href="https://twitter.com/JohnPevien
+            "
+              rel="noreferrer"
+            >
+              <AiOutlineTwitter size={"2em"} />
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
