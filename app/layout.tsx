b1a993f01624a51959a1cda0ae659f5c06f488fc
John Pevien Laguindam <johnpevienwork@gmail.com>
2024-05-27 01:39:23 +0800

    chore: Add next-themes for manual theme toggle switching and update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class">
+          <div className="bg-transparent dark:bg-neutral w-full min-h-[90vh]">
+            {children}
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
